3baba84b801a7f174b9457d4a0cf25c24244b29c
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2025-07-13 03:54:04 +0700

    fix(save): update all items in struct to expose by change to Pascal case

--- single-note/note/note.go
+++ single-note/note/note.go
@@ -10,18 +10,18 @@ import (
 )
 
 type Note struct {
-	title     string
-	content   string
-	createdAt time.Time
+	Title     string
+	Content   string
+	CreatedAt time.Time
 }
 
 func (n Note) Display() {
-	fmt.Printf("Your note titled %v has the following content: \n\n%v\n\n", n.title, n.content)
+	fmt.Printf("Your note titled %v has the following content: \n\n%v\n\n", n.Title, n.Content)
 }
 
 func (n Note) Save() error {
-	fileName := strings.ReplaceAll(n.title, " ", "_")
-	fileName = strings.ToLower(fileName)
+	fileName := strings.ReplaceAll(n.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
 
 	json, err := json.Marshal(n)
 
@@ -29,7 +29,7 @@ func (n Note) Save() error {
 		return err
 	}
 
-	return os.WriteFile(fileName+".json", json, 0644)
+	return os.WriteFile(fileName, json, 0644)
 }
 
 func New(title, content string) (Note, error) {
@@ -41,8 +41,8 @@ func New(title, content string) (Note, error) {
 	}
 
 	return Note{
-		title:     title,
-		content:   content,
-		createdAt: time.Now(),
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
 	}, nil
 }
