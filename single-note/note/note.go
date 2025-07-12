0c9af0847c852cfd6c4aa6672f87f328547675af
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2025-07-12 17:51:11 +0700

    feat(save): create method to save note as file but still got empty file

--- single-note/note/note.go
+++ single-note/note/note.go
@@ -1,8 +1,11 @@
 package note
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -14,6 +17,19 @@ type Note struct {
 
 func (n Note) Display() {
 	fmt.Printf("Your note titled %v has the following content: \n\n%v\n\n", n.title, n.content)
+}
+
+func (n Note) Save() error {
+	fileName := strings.ReplaceAll(n.title, " ", "_")
+	fileName = strings.ToLower(fileName)
+
+	json, err := json.Marshal(n)
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName+".json", json, 0644)
 }
 
 func New(title, content string) (Note, error) {
